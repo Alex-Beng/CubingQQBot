b919b53c281628e4c436e364dbcdd6ae15272f2c
Alex Beng <30763045+Alex-Beng@users.noreply.github.com>
2018-01-31 11:36:14 +0800

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,1 +1,32 @@
 import Spider
+
+# input raw Message
+# output ( flag , proedMsg  )
+# flag == 0 is nothing
+# flag == 1 is refer person
+# flag == 2 is get scramble
+def procMsg(rawMsg):
+    if rawMsg[0] == '-':
+        tList = rawMsg.split('-')
+        if tList[1] == '' :
+            return   0, ''
+        else :
+            if '-wca' in rawMsg:
+                tList = rawMsg.split('-wca')
+                return 1,tList[1]
+            # Not complite the disorganize part !
+            else:
+                if '-3' in rawMsg:
+                    return 2, '333'
+                elif '-2' in rawMsg:
+                    return 2, '222'
+                elif '-5' in rawMsg:
+                    return 2,'555'
+
+def sendMsg2Group(msg,groupList,bot):
+    for group in groupList:
+        bg = bot.List('group',group)
+        if bg is not None:
+            bot.SendTo (bg[0],msg)
+
+
